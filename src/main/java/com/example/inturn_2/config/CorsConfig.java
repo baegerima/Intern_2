dd5d3f262e3e0fa78fb48e5861381d611764a2c8
baegerima <aigerimnaizabekova25@gmail.com>
2025-02-20 15:03:31 +0600

    database

--- src/main/java/com/example/inturn_2/config/CorsConfig.java
+++ src/main/java/com/example/inturn_2/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://transcriptfront-62cad49a4e47.herokuapp.com/") // два URL
+                .allowedOrigins("https://transcriptfront-62cad49a4e47.herokuapp.com") // два URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
 
